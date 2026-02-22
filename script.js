12c31af84e7af7623ec32c897fa1442d239f466f
Marcos-Padilla <56989957+Marco-Padilla@users.noreply.github.com>
2026-02-21 18:15:57 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("menu-toggle");
-  const navUl = document.querySelector("nav ul");
-
-  menuToggle.addEventListener("click", function () {
-    navUl.classList.toggle("show");
-  });
-});
